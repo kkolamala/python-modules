5f85d8918b5c1169f6a3b236f0b33be4129de130
kolamala <mohan.kolamala@gmail.com>
2021-04-18 13:33:18 -0700

    python modules

---
+++ package1/file1.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('./package2')
+import file2
+# from file2 import printFirstName
+# from file2 import printFirstName,printFullName
+# from file2 import *
+
+
+
+# printFullName('Krishna','Kolamala')
+
+# printFirstName('Krishna')
+
+print(dir(file2)) # to list all the functions in a module
+
---
+++ package2/file2.py
@@ -0,0 +1,7 @@
+def printFullName(firstName , lastName):
+    print('full name is {0} {1}'.format(firstName,lastName))
+
+
+def printFirstName(firstName):
+    print('first name is {0}'.format(firstName))
+        
